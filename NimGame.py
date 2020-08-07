21b5ad7959910ad17851a248acf30a588107ec8d
Anshuma Jain <anshumajain9@gmail.com>
2020-08-06 17:16:26 -0700

    update from python 2 ->3; added comments; fixed PEP8; cleaned up code;

--- NimGame.py
+++ NimGame.py
@@ -1,13 +1,30 @@
+"""
+LeetCode Problem #292: Nim Game
+You are playing the following Nim Game with your friend: There is a heap of stones on
+the table, each time one of you take turns to remove 1 to 3 stones. The one who removes
+the last stone will be the winner. You will take the first turn to remove the stones.
 
-def nimGame(n): #no. of stones
+Both of you are very clever and have optimal strategies for the game. Write a function
+to determine whether you can win the game given the number of stones in the heap.
 
-    #base case 4
-    if n % 4 == 0:
+Example:
+Input: 4
+Output: false
+Explanation: If there are 4 stones in the heap, then you will never win the game;
+             No matter 1, 2, or 3 stones you remove, the last stone will always be
+             removed by your friend.
+"""
+
+
+def nim_game(num_of_stones):
+
+    # Base case 4
+    if num_of_stones % 4 == 0:
         return False
     else:
         return True
 
 
-
-Number = input("enter the number of stones = ")
-print (nimGame(Number))
+if __name__ == "__main__":
+    total_num_of_stones = int(input("Enter the total number of stones = "))
+    print(nim_game(total_num_of_stones))
