55ab6112d05415cd417c8182483d4db98d0b9901
Anshuma Jain <anshumajain9@gmail.com>
2020-10-03 21:28:00 -0700

    cleaned up comments

--- TicTacToegame.py
+++ TicTacToegame.py
@@ -5,14 +5,14 @@ Tic-tac-toe(ttt) game graphical user interface using pygame and class inheritanc
 import pygame
 
 # Colors
-BG_SCREEN_COLOR = (32, 178, 170)  # LIGHT SEA GREEN
-O_COLOR = (255, 255, 255)  # WHITE
-X_COLOR = (0, 0, 0)  # BLACK
-OX_DRAW_COLOR = (105, 105, 105)  # GRAY
-LINE_COLOR = (0, 128, 128)  # TEAL
-RESTART_BG_COLOR = (255, 255, 255)   # WHITE
-RESTART_FONT_COLOR_ACTIVE = (0, 128, 128) # TEAL
-RESTART_FONT_COLOR_PASSIVE = (32, 178, 170)  # LIGHT SEA GREEN
+BG_SCREEN_COLOR = (32, 178, 170)                # LIGHT SEA GREEN
+O_COLOR = (255, 255, 255)                       # WHITE
+X_COLOR = (0, 0, 0)                             # BLACK
+OX_DRAW_COLOR = (105, 105, 105)                 # GRAY
+LINE_COLOR = (0, 128, 128)                      # TEAL
+RESTART_BG_COLOR = (255, 255, 255)              # WHITE
+RESTART_FONT_COLOR_ACTIVE = (0, 128, 128)       # TEAL
+RESTART_FONT_COLOR_PASSIVE = (32, 178, 170)     # LIGHT SEA GREEN
 
 # Display Pixel Area
 SCREEN_SIZE = [300, 300]
@@ -36,16 +36,16 @@ CELL_CENTER_HASH_MAP = {
 
 # Maps column pixel border to Cell No.
 X_BORDER_TO_CELL_MAP = {
-    (120, 60): {1, 4, 7},   # cells in 1st col
-    (180, 120): {2, 5, 8},  # cells in 2nd col
-    (240, 180): {3, 6, 9}   # cells in 3rd col
+    (120, 60): {1, 4, 7},               # cells in 1st col
+    (180, 120): {2, 5, 8},              # cells in 2nd col
+    (240, 180): {3, 6, 9}               # cells in 3rd col
 }
 
 # Maps row pixel border to Cell No.
 Y_BORDER_TO_CELL_MAP = {
-    (60, 120): {1, 2, 3},   # cells in 1st row
-    (120, 180): {4, 5, 6},  # cells in 2nd row
-    (180, 240): {7, 8, 9}   # cells in 3rd row
+    (60, 120): {1, 2, 3},               # cells in 1st row
+    (120, 180): {4, 5, 6},              # cells in 2nd row
+    (180, 240): {7, 8, 9}               # cells in 3rd row
 }
 
 # Cell combos needed to win
@@ -59,26 +59,26 @@ WINNING_CELL_COMBOS = {
 class Gui:
 
     def __init__(self):
-        pygame.init()                                           #  nitialize all imported pygame modules
+        pygame.init()                                           # initialize all imported pygame modules
         self.font = pygame.font.Font('freesansbold.ttf', 25)    # default font
-        self.screen = pygame.display.set_mode(SCREEN_SIZE)      # Set up the drawing window
-        self.screen.fill(BG_SCREEN_COLOR)                       # Fill the background color
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)      # set up the drawing window
+        self.screen.fill(BG_SCREEN_COLOR)                       # fill the background color
         self.draw_initial_grid_and_labels()                     # draw all the initial labels and the grid
 
 
     def draw_initial_grid_and_labels(self):
         """ set window title; draw the labels and the tic-tac-toe grid """
 
-        pygame.display.set_caption('Welcome to Tic-Tac-Toe game')  # set pygame window title
+        pygame.display.set_caption('Welcome to Tic-Tac-Toe game')                   # set pygame window title
 
         # Draw the tic-tac-toe hatched lines
-        pygame.draw.line(self.screen, LINE_COLOR, (120, 60), (120, 240), 5)  # vertical line1
-        pygame.draw.line(self.screen, LINE_COLOR, (180, 60), (180, 240), 5)  # vertical line2
-        pygame.draw.line(self.screen, LINE_COLOR, (60, 120), (240, 120), 5)  # horizontal line1
-        pygame.draw.line(self.screen, LINE_COLOR, (60, 180), (240, 180), 5)  # horizontal line2
+        pygame.draw.line(self.screen, LINE_COLOR, (120, 60), (120, 240), 5)         # vertical line1
+        pygame.draw.line(self.screen, LINE_COLOR, (180, 60), (180, 240), 5)         # vertical line2
+        pygame.draw.line(self.screen, LINE_COLOR, (60, 120), (240, 120), 5)         # horizontal line1
+        pygame.draw.line(self.screen, LINE_COLOR, (60, 180), (240, 180), 5)         # horizontal line2
 
         # display the initial turn text
-        turn_text = self.font.render(" O Turn ", True, O_COLOR, BG_SCREEN_COLOR)  # O label
+        turn_text = self.font.render(" O Turn ", True, O_COLOR, BG_SCREEN_COLOR)    # label for initial turn
         turn_text_rect = turn_text.get_rect()
         turn_text_rect.center = (150, 25)
         self.screen.blit(turn_text, turn_text_rect)
@@ -91,15 +91,17 @@ class Gui:
     def display_symbol(self, symbol, cell_num):
         """ function to draw the 'X' and 'O' symbols in the given cell no."""
 
-        x, y = CELL_CENTER_HASH_MAP[cell_num]   # get center coordinates of the given cell no.
+        x, y = CELL_CENTER_HASH_MAP[cell_num]                               # get center coordinates of the given cell no.
 
-        if not self.is_game_over:   # if the game isn't over yet
+        if not self.is_game_over:                                           # if the game isn't over yet
             if symbol == 'O':
                 symbol_color = O_COLOR
-                turn_text = self.font.render(' X Turn ', True, X_COLOR, BG_SCREEN_COLOR)  # display the next turn
+                turn_text = self.font.render(' X Turn ', True,
+                                             X_COLOR, BG_SCREEN_COLOR)      # display the next turn
             else:
                 symbol_color = X_COLOR
-                turn_text = self.font.render(" O Turn ", True, O_COLOR, BG_SCREEN_COLOR)  # display the next turn
+                turn_text = self.font.render(" O Turn ", True,
+                                             O_COLOR, BG_SCREEN_COLOR)      # display the next turn
 
             # draw the 'X' or 'O' symbol on the grid
             symbol_text = self.font.render(symbol, True, symbol_color)
@@ -125,13 +127,13 @@ class Gui:
     def display_game_result(self, win_symbol, winning_cells=None):
         """ function to display the winning screen """
 
-        if win_symbol == 'XO':  # if there is a tie
+        if win_symbol == 'XO':                                      # if there is a tie
             self.display_win_message("X-O Draw!", OX_DRAW_COLOR)
         else:
-            if win_symbol == 'O': # if 'O' has won
+            if win_symbol == 'O':                                   # if 'O' has won
                 color = O_COLOR
                 win_message = 'O Wins!'
-            elif win_symbol == 'X':  # if 'X' has won
+            elif win_symbol == 'X':                                 # if 'X' has won
                 color = X_COLOR
                 win_message = 'X Wins!'
 
@@ -143,30 +145,30 @@ class Gui:
             pygame.draw.line(self.screen, color, cord2, cord3, 3)
 
 
-class TicTacToe(Gui):   # inherited from Gui class
+class TicTacToe(Gui):                               # inherited from Gui class
 
     def __init__(self):
-        Gui.__init__(self) # call the parent class constructor
-        self.turn = 0  # variable to count the turns (1-9)
-        self.is_game_over = False  # boolean to monitor if the game is over
-        self.cell = [None] * 9  # cell array representing 9 cells of the tic-tac-toe grid
+        Gui.__init__(self)                          # call the parent class constructor
+        self.turn = 0                               # variable to count the turns (1-9)
+        self.is_game_over = False                   # boolean to monitor if the game is over
+        self.cell = [None] * 9                      # cell array representing 9 cells of the tic-tac-toe grid
 
 
     def restart_game(self):
-        self.turn = 0   # reset the turns
-        self.is_game_over = False  # reset game_over boolean
-        self.cell = [None] * 9  # reinitialize the tic-tac-toe cell array
-        super().__init__() # call parent class constructor to redraw the GUI
+        self.turn = 0                               # reset the turns
+        self.is_game_over = False                   # reset game_over boolean
+        self.cell = [None] * 9                      # reinitialize the tic-tac-toe cell array
+        super().__init__()                          # call parent class constructor to redraw the GUI
 
 
     def process_player_turn(self, cell_no):
         """ function to process the turn """
-        if self.turn % 2 == 0:  # if the turn is even
+        if self.turn % 2 == 0:                      # if the turn is even
             symbol = 'O'
         else:
             symbol = 'X'
 
-        if not self.cell[cell_no - 1]: # if the clicked cell isn't already filled
+        if not self.cell[cell_no - 1]:              # if the clicked cell isn't already filled
             self.display_symbol(symbol, cell_no)
             self.turn += 1
             self.cell[cell_no - 1] = symbol
@@ -238,9 +240,9 @@ class TicTacToe(Gui):   # inherited from Gui class
                 text = restart_font.render("Restart Game", True, RESTART_FONT_COLOR_PASSIVE)
             text_rect = text.get_rect()
             text_rect.center = (150, 280)
-            self.screen.blit(text, text_rect) # redraw the text on screen
+            self.screen.blit(text, text_rect)                           # redraw the text on screen
 
-            pygame.display.update() # update display
+            pygame.display.update()                                     # update display
 
 
 if __name__ == "__main__":
